beb1693f6ceb5c6d1522099e2a2d226952518fd6
PismarovMikhail <mikepiamarov@gmail.com>
2024-01-23 01:28:11 +0300

    Create evaluate step (prediction)

--- scr/evaluate.py
+++ scr/evaluate.py
@@ -1,37 +1,53 @@
 import sys
 import os
-import yaml
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import precision_recall_curve, auc
 import pickle
+import json
 
-# read the command line params
-if len(sys.argv) != 3:
+# read command line parameters
+if len(sys.argv) != 5:
     sys.stderr.write('Arguments error. Usage:\n')
     sys.stderr.write(
-        '\tpython3 train.py features-dir-path model-filename\n'
+        '\tpython3 evaluate.py model-filename features-dir-path scores-filename\
+                plots-filename\n'
     )
     sys.exit(1)
 
-features_path = sys.argv[1]
-model_filename = sys.argv[2]
+model_filename = sys.argv[1]
+features_path = sys.argv[2]
+test_features_file = os.path.join(os.path.join(features_path, 'test.pkl'))
+scores_file = sys.argv[3]
+plots_file = sys.argv[4]
 
-# read pipeline params
-params = yaml.safe_load(open('params.yaml'))['train']
+# load features
+with open(test_features_file, 'rb') as f:
+    test_features = pickle.load(f)
+    
+X_test = test_features.iloc[:,:-1]
+y_test = test_features.iloc[:,-1]
 
-alpha = params['alpha']
+# load model
+with open(model_filename, 'rb') as f:
+    model = pickle.load(f)
 
-# load the train features
-features_train_pkl = os.path.join(features_path, 'train.pkl')
-with open(features_train_pkl, 'rb') as f:
-    train_data = pickle.load(f)
+# make predictions
+predictions_by_class = model.predict_proba(X_test)
+predictions = predictions_by_class[:,-1]
 
-X = train_data.iloc[:,:-1]
-y = train_data.iloc[:,-1]
+# generate scores
+precision, recall, thresholds = precision_recall_curve(y_test, predictions)
+auc = auc(recall, precision)
 
-# train the model
-clf = MultinomialNB(alpha=alpha)
-clf.fit(X, y)
+# save scores
+with open(scores_file, 'w') as f:
+    json.dump({'auc': auc}, f)
 
-# save the model
-with open(model_filename, 'wb') as f:
-    pickle.dump(clf, f)
+# save plots
+with open(plots_file, 'w') as f:
+    proc_dict = {'proc': [{
+        'precision': p,
+        'recall': r,
+        'threshold': t
+        } for p, r, t in zip(precision, recall, thresholds)
+    ]}
+    json.dump(proc_dict, f)
